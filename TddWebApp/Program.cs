5d62814818db3bc687e2530e12d2604fa264b160
Xavier Pujol <xavier.pujol@ohtic.com>
2023-10-16 16:42:11 +0200

    Update Program.cs

--- TddWebApp/Program.cs
+++ TddWebApp/Program.cs
@@ -11,12 +11,16 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
