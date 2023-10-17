47f0c1a066866614376bb48683adda8bbc0268a9
Xavier Pujol <xavier.pujol@ohtic.com>
2023-10-17 12:26:12 +0200

    d

---
+++ TddWebApp/DiazService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Collections.Generic;
+using TddWebApp;
+
+namespace TddApp
+{
+    public class DiazService
+    { 
+    //public int MySuperLogic(int a, int b, int c, int d)
+    //{
+    //    var sourceArray = new List<int>() { a, b, c, d };
+    //    ValidateValues(sourceArray);
+    //    ValidateArray(sourceArray);
+    //    var res = sourceArray.Max() - sourceArray.Min() + 1;
+    //    ValidateResult(a,b,c,d,res);
+    //    return res;
+    //}
+
+}
+}
---
+++ TddWebAppTest/EDiazLogicServiceTest.cs
@@ -0,0 +1,22 @@
+using System.Net;
+using TddApp;
+using TddWebApp;
+
+namespace TddWebAppTest
+{
+    public class EDiazLogicServiceTest
+    {
+        [Fact]
+        public void Get()
+        {
+            // Arrange
+            LogicService logicService = new LogicService();
+
+            // Act
+            
+
+            // Assert
+            //Assert.Equal(999, res);
+        }
+    }
+}
