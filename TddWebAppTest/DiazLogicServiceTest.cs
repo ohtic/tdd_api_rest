ae7bde4c28de12c78a195492752aa1ae25937d8e
Sergio Diaz <devsdm99@gmail.com>
2023-10-17 16:59:44 +0200

    testaso

--- TddWebAppTest/DiazLogicServiceTest.cs
+++ TddWebAppTest/DiazLogicServiceTest.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+using System;
 using System.Net;
 using TddApp;
 using TddWebApp;
@@ -7,16 +9,41 @@ namespace TddWebAppTest
     public class DiazLogicServiceTest
     {
         [Fact]
-        public void Get()
+        public void CorrectInput_Returns_CorrectValue()
         {
             // Arrange
-            LogicService logicService = new LogicService();
-
+            IDiazService logicService = new DiazService();
             // Act
-            
+            var res = logicService.MySuperLogic(11, 22, 33, 44);
 
             // Assert
-            //Assert.Equal(999, res);
+            Assert.Equal(34, res.Result);
+
+        }
+
+        [Fact]
+        public void WrongInput_Throws_Exception()
+        {
+            // Arrange
+            IDiazService logicService = new DiazService();
+            int wrongNumber = -2;
+            // Act
+            var ex = Assert.Throws<Exception>(() => logicService.MySuperLogic(wrongNumber, 666, 13, 1423));
+
+            // Assert
+            Assert.Equal($"El número {wrongNumber} no es valido revise su ouija de confianza para más información.", ex.Message);
+        }
+
+        [Fact]
+        public void Input_Contains_DiscardValues_Exception()
+        {
+            // Arrange
+            IDiazService logicService = new DiazService();
+            // Act
+            var ex = Assert.Throws<Exception>(() => logicService.MySuperLogic(10, 22, 33, 44));
+
+            // Assert
+            Assert.Equal("Los números 13 y 10 causan un error por mala suerte.", ex.Message);
         }
     }
 }
