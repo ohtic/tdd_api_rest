4d599dc7e3276d8e237ed327a85b08f26871cd8a
Augusto Casano <augusto.casano@ohtic.com>
2023-10-17 16:46:36 +0200

    Added "Los números no pueden estar duplicados" functionality

--- TddWebApp/CiroService.cs
+++ TddWebApp/CiroService.cs
@@ -6,6 +6,7 @@
         {
             var sourceArray = new List<int>() { a, b, c, d };
             ValidateValues(sourceArray);
+            ValidateArray(sourceArray);
             var res = sourceArray.Max() - sourceArray.Min() + 1;
 
             return res;
@@ -14,6 +15,11 @@
         private void ValidateValues(List<int> array)
         {
             if (array.Exists(x => x.ToString().Length > 4)) throw new LogicValidationException(410, "El valor solo puede ser un int de máximo 4 cifras");
+        }
+
+        private void ValidateArray(List<int> array)
+        {
+            if (array.GroupBy(x => x).Any(g => g.Count() > 1)) throw new LogicValidationException(418, "Los números no pueden estar duplicados");
         }
     }
 
