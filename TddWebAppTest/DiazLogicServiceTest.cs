359f27926861c7537f043dc64c32b3af54b76372
Sergio Diaz <devsdm99@gmail.com>
2023-10-17 16:59:44 +0200

    testaso

--- TddWebAppTest/DiazLogicServiceTest.cs
+++ TddWebAppTest/DiazLogicServiceTest.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+using System;
 using System.Net;
 using TddApp;
 using TddWebApp;
@@ -7,16 +9,41 @@ namespace TddWebAppTest
     public class DiazLogicServiceTest
     {
         [Fact]
-        public void Get()
+        public void CorrectInput_Returns_CorrectValue()
         {
             // Arrange
-            LogicService logicService = new LogicService();
-
+            IDiazService logicService = new DiazService();
             // Act
-            
+            var res = logicService.MySuperLogic(11, 22, 33, 44);
 
             // Assert
-            //Assert.Equal(999, res);
+            Assert.Equal(34, res.Result);
+
+        }
+
+        [Fact]
+        public void WrongInput_Throws_Exception()
+        {
+            // Arrange
+            IDiazService logicService = new DiazService();
+            int wrongNumber = -2;
+            // Act
+            var ex = Assert.Throws<Exception>(() => logicService.MySuperLogic(wrongNumber, 666, 13, 1423));
+
+            // Assert
+            Assert.Equal($"The number {wrongNumber} is not in the range of 1 to 10000", ex.Message);
+        }
+
+        [Fact]
+        public void Input_Contains_DiscardValues_Exception()
+        {
+            // Arrange
+            IDiazService logicService = new DiazService();
+            // Act
+            var ex = Assert.Throws<Exception>(() => logicService.MySuperLogic(10, 22, 33, 44));
+
+            // Assert
+            Assert.Equal("The array contains invalid numbers", ex.Message);
         }
     }
 }
