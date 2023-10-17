70ed1bfad25c154ad159b9bb17d63b649bcbf2cb
Augusto Casano <augusto.casano@ohtic.com>
2023-10-17 16:50:54 +0200

    Added "Los números 13 y 10 causan un error por mala suerte" functionality

--- TddWebApp/CiroService.cs
+++ TddWebApp/CiroService.cs
@@ -17,6 +17,9 @@
             if (array.Exists(x => x.ToString().Length > 4)) throw new LogicValidationException(410, "El valor solo puede ser un int de máximo 4 cifras");
 
             if (array.Exists(x => x == 666)) throw new LogicValidationException(450, "El número 666 no es valido revise su ouija de confianza para más información");
+
+            var toExclude = new List<int>() { 13, 10 };
+            if (array.Where(x => toExclude.Contains(x)).Any()) throw new LogicValidationException(555, "Los números 13 y 10 causan un error por mala suerte");
         }
 
         private void ValidateArray(List<int> array)
