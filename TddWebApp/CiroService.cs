b7ce5c44c791903e556de6ec8f0021f91235b5ef
Augusto Casano <augusto.casano@ohtic.com>
2023-10-17 16:43:08 +0200

    Added "El valor solo puede ser un int de máximo 4 cifras" functionality

--- TddWebApp/CiroService.cs
+++ TddWebApp/CiroService.cs
@@ -4,10 +4,28 @@
     {
         public int MySuperLogic(int a, int b, int c, int d)
         {
-            var sourceArray = new List<int>() { a, b, c, d }; ;
+            var sourceArray = new List<int>() { a, b, c, d };
+            ValidateValues(sourceArray);
             var res = sourceArray.Max() - sourceArray.Min() + 1;
 
             return res;
         }
+
+        private void ValidateValues(List<int> array)
+        {
+            if (array.Exists(x => x.ToString().Length > 4)) throw new LogicValidationException(410, "El valor solo puede ser un int de máximo 4 cifras");
+        }
+    }
+
+    public class LogicValidationException : Exception
+    {
+        public LogicValidationException(int code, string message)
+        {
+            Code = code;
+            Message = message;
+        }
+
+        public int Code { get; }
+        public new string Message { get; }
     }
 }
