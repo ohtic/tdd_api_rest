c1693d9f757c957dc47f831a08e3b9c8dbd2df2c
AlvaroEsteve <alvaro.esteve@ohtic.com>
2023-10-17 16:34:30 +0200

    Test 1

---
+++ TddWebApp/EsteveService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Collections.Generic;
+using TddWebApp;
+
+namespace TddApp
+{
+    public class EsteveService
+    {
+        public int MySuperLogic(int a, int b, int c, int d)
+        {      
+            var list = new List<int>() { a, b, c, d };
+            return list.Max() - list.Min() + 1;
+        }
+    }
+}
---
+++ TddWebAppTest/EsteveLogicServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using TddApp;
+using TddWebApp;
+
+namespace TddWebAppTest
+{
+    public class EsteveLogicServiceTest
+    {
+        [Fact]
+        public void Get_OK()
+        {
+            // Arrange
+            EsteveService logicService = new EsteveService();
+
+            // Act
+            var res = logicService.MySuperLogic(1, 2, 3, 4);
+
+            // Assert
+            Assert.Equal(4, res);
+        }
+    }
+}
